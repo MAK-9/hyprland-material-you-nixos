b9f4417886d80ecb7742a95ddc77a835212e0a2d
MAK-9 <13michal9@gmail.com>
2026-05-21 15:10:09 +0200

    Fix wrapper heredoc: escape \$PATH, \$HOME, \$@ to prevent build-time expansion
    Using unquoted << WRAPEOF caused bash to expand variables during nix build:
    - \$PATH expanded to the massive build-time PATH (including ./.hypryou/bin etc.)
    - \$HOME expanded to /homeless-shelter (nix build fake home)
    - \$@ expanded to "installPhase" (nix phase name)
    
    Escaping them with \$ produces correct literal runtime variables in the script.

--- flake.nix
+++ flake.nix
@@ -145,11 +145,11 @@ COLEOF
             mv $out/bin/hypryou-start $out/bin/.hypryou-start-unwrapped
             cat > $out/bin/hypryou-start << WRAPEOF
 #!/bin/sh
-export PATH="${pythonEnv}/bin:$PATH"
+export PATH="${pythonEnv}/bin:\$PATH"
 export GI_TYPELIB_PATH="${giTypelibPath}"
 export LD_LIBRARY_PATH="${giLibPath}"
-export XDG_DATA_DIRS="/run/current-system/sw/share:$HOME/.nix-profile/share:/usr/local/share:/usr/share"
-exec $out/bin/.hypryou-start-unwrapped "''$@"
+export XDG_DATA_DIRS="/run/current-system/sw/share:\$HOME/.nix-profile/share:/usr/local/share:/usr/share"
+exec $out/bin/.hypryou-start-unwrapped "\$@"
 WRAPEOF
             chmod +x $out/bin/hypryou-start
 
